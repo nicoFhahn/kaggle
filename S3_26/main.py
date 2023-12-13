537d47a39651bdd8ccf089c0e025f16a4f4a71ee
nicohahn <nico.hahn@park-depot.de>
2023-12-13 12:36:31 +0100

    first models done

--- S3_26/main.py
+++ S3_26/main.py
@@ -1,56 +1,62 @@
 import jenkspy
 import pandas as pd
 import seaborn as sns
+import xgboost as xgb
+import numpy as np
+import scipy.stats as stats
 from sklearn.naive_bayes import BernoulliNB
-from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier, IsolationForest
-from sklearn.neighbors import KNeighborsClassifier, NearestCentroid, LocalOutlierFactor
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier, LocalOutlierFactor
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
 from sklearn.svm import LinearSVC
-from sklearn.linear_model import LogisticRegression, LogisticRegressionCV, RidgeClassifier, RidgeClassifierCV
+from sklearn.linear_model import LogisticRegression, RidgeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss
+from sklearn.preprocessing import LabelEncoder
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import HalvingRandomSearchCV
+from imblearn.over_sampling import SMOTE, ADASYN
+from collections import Counter
 from tqdm import tqdm
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
-train = pd.read_csv("train.csv")
+# load the training data
+train = pd.read_csv("S3_26/train.csv")
 df = train.copy()
-train = pd.concat([train, pd.get_dummies(train.Drug)], axis=1).drop(columns="Drug")
-train = pd.concat([train, pd.get_dummies(train.Sex)], axis=1).drop(columns="Sex")
-train = pd.concat([train, pd.get_dummies(train.Ascites)], axis=1).drop(columns="Ascites")
-train = pd.concat([train, pd.get_dummies(train.Hepatomegaly)], axis=1).drop(columns="Hepatomegaly")
-train = pd.concat([train, pd.get_dummies(train.Spiders)], axis=1).drop(columns="Spiders")
-train = pd.concat([train, pd.get_dummies(train.Edema)], axis=1).drop(columns="Edema")
-X, y = train.drop(columns=["id", "Status"]), train.Status
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=420)
-
-models = [BernoulliNB(), ExtraTreesClassifier(), RandomForestClassifier(), KNeighborsClassifier(),
-          LinearDiscriminantAnalysis(), QuadraticDiscriminantAnalysis(), LinearSVC(multi_class="crammer_singer"),
-          LogisticRegression(multi_class="multinomial"), RidgeClassifier()]
-model_1 = BernoulliNB().fit(X_train, y_train)
-log_loss_1 = log_loss(y_test, model_1.predict_proba(X_test))
-model_2 = ExtraTreesClassifier().fit(X_train, y_train)
-log_loss_2 = log_loss(y_test, model_2.predict_proba(X_test))
-model_3 = RandomForestClassifier().fit(X_train, y_train)
-log_loss_3 = log_loss(y_test, model_3.predict_proba(X_test))
-model_4 = KNeighborsClassifier().fit(X_train, y_train)
-log_loss_4 = log_loss(y_test, model_4.predict_proba(X_test))
-model_5 = LinearDiscriminantAnalysis().fit(X_train, y_train)
-log_loss_5 = log_loss(y_test, model_5.predict_proba(X_test))
-model_6 = QuadraticDiscriminantAnalysis().fit(X_train, y_train)
-log_loss_6 = log_loss(y_test, model_6.predict_proba(X_test))
-model_7 = LinearSVC(multi_class="crammer_singer").fit(X_train, y_train)
-log_loss_7 = log_loss(y_test, model_7._predict_proba_lr(X_test))
-model_8 = LogisticRegression(multi_class="multinomial").fit(X_train, y_train)
-log_loss_8 = log_loss(y_test, model_8.predict_proba(X_test))
-model_9 = LogisticRegressionCV(multi_class="multinomial").fit(X_train, y_train)
-log_loss_9 = log_loss(y_test, model_9.predict_proba(X_test))
-model_10 = RidgeClassifier().fit(X_train, y_train)
-log_loss_10 = log_loss(y_test, model_10._predict_proba_lr(X_test))
-model_11 = RidgeClassifierCV().fit(X_train, y_train)
-log_loss_11 = log_loss(y_test, model_11._predict_proba_lr(X_test))
-
+# turn stage into category
+train["Stage"] = train.Stage.astype("category")
+# add dummy variables
+train = pd.concat([train, pd.get_dummies(train.Drug, prefix="Drug")], axis=1).drop(columns="Drug")
+train = pd.concat([train, pd.get_dummies(train.Sex, prefix="Sex")], axis=1).drop(columns="Sex")
+train = pd.concat([train, pd.get_dummies(train.Ascites, prefix="Ascites")], axis=1).drop(columns="Ascites")
+train = pd.concat([train, pd.get_dummies(train.Hepatomegaly, prefix="Hepatomegaly")], axis=1).drop(columns="Hepatomegaly")
+train = pd.concat([train, pd.get_dummies(train.Spiders, prefix="Spiders")], axis=1).drop(columns="Spiders")
+train = pd.concat([train, pd.get_dummies(train.Edema, prefix="Edema")], axis=1).drop(columns="Edema")
+train = pd.concat([train, pd.get_dummies(train.Stage, prefix="Stage")], axis=1).drop(columns="Stage")
+# encode status
+le = LabelEncoder()
+# split into X & y
+X, y = train.drop(columns=["id", "Status"]), le.fit_transform(train.Status)
+# split into train & test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=420)
+# default models and proba function
+models = [
+    (BernoulliNB(), "proba"), (ExtraTreesClassifier(random_state=42), "proba"),
+    (RandomForestClassifier(random_state=42), "proba"), (KNeighborsClassifier(), "proba"),
+    (LinearDiscriminantAnalysis(), "proba"), (QuadraticDiscriminantAnalysis(), "proba"),
+    (LinearSVC(multi_class="crammer_singer"), "proba_lr"), (LogisticRegression(multi_class="multinomial"), "proba"),
+    (RidgeClassifier(), "proba_lr"), (xgb.XGBClassifier(objective='multi:softmax', seed=42), "proba")
+]
+# calculate log loss for default models
+log_losses = [
+    log_loss(y_test, models[i][0].fit(X_train, y_train).predict_proba(X_test))
+    if models[i][1] == "proba" else
+    log_loss(y_test, models[i][0].fit(X_train, y_train)._predict_proba_lr(X_test))
+    for i in tqdm(range(len(models)))
+]
+# replace outliers with max value
 for col in ["SGOT", "Tryglicerides", "Prothrombin"]:
     fit = LocalOutlierFactor(n_neighbors=20, contamination=0.1).fit_predict(X_train[[col]])
     temp_df = pd.concat([X_train[[col]].reset_index(drop=True), pd.Series(fit)], axis=1)
@@ -60,36 +66,236 @@ for col in ["SGOT", "Tryglicerides", "Prothrombin"]:
     X_train.loc[X_train[col] >= max_value, col] = max_value
     X_test.loc[X_test[col] <= min_value, col] = min_value
     X_test.loc[X_test[col] >= max_value, col] = max_value
-jenks_dict = {
-    "Bilirubin": 3,
-    "Cholesterol": 2,
-    "Copper": 3,
-    "Alk_Phos": 3
-}
-for key, value in jenks_dict.items():
-    breaks = jenkspy.jenks_breaks(X_train[key], n_classes=value)
-    cat = pd.cut(X_train[key], bins=breaks)
-    X_train[key + "_cat"] = cat
-    cat = pd.cut(X_test[key], bins=breaks)
-    X_test[key + "_cat"] = cat
-
-X_train["Stage"] = X_train.Stage.astype("category")
-X_train = X_train.copy()
-X_train_2 = X_train[X_train.columns[:-4]]
-fitted_models = [m.fit(X_train_2, y_train) for m in tqdm(models)]
-X_train_3 = X_train.drop(columns=jenks_dict.keys())
-X_train_3 = pd.concat([X_train_3, pd.get_dummies(X_train_3.Bilirubin_cat)], axis=1).drop(columns="Bilirubin_cat")
-X_train_3 = pd.concat([X_train_3, pd.get_dummies(X_train_3.Cholesterol_cat)], axis=1).drop(columns="Cholesterol_cat")
-X_train_3 = pd.concat([X_train_3, pd.get_dummies(X_train_3.Copper_cat)], axis=1).drop(columns="Copper_cat")
-X_test_2 = X_test.copy()
-X_test_2 = pd.concat([X_test_2, pd.get_dummies(X_test_2.Alk_Phos_cat)], axis=1).drop(columns="Alk_Phos_cat")
-X_test_2 = pd.concat([X_test_2, pd.get_dummies(X_test_2.Bilirubin_cat)], axis=1).drop(columns="Bilirubin_cat")
-X_test_2 = pd.concat([X_test_2, pd.get_dummies(X_test_2.Cholesterol_cat)], axis=1).drop(columns="Cholesterol_cat")
-X_test_2 = pd.concat([X_test_2, pd.get_dummies(X_test_2.Copper_cat)], axis=1).drop(columns="Copper_cat")
-X_test_2 = pd.concat([X_test_2, pd.get_dummies(X_test_2.Alk_Phos_cat)], axis=1).drop(columns="Alk_Phos_cat")
-fitted_models = [m.fit(X_train_3, y_train) for m in tqdm(models)]
+#jenks_dict = {
+#    "Bilirubin": 3,
+#    "Cholesterol": 2,
+#    "Copper": 3,
+#    "Alk_Phos": 3
+#}
+#for key, value in jenks_dict.items():
+#    breaks = jenkspy.jenks_breaks(X_train[key], n_classes=value)
+#    cat = pd.cut(X_train[key], bins=breaks).astype("interval")
+#    X_train[key + "_cat"] = cat
+#    cat = pd.cut(X_test[key], bins=breaks).astype("interval")
+#    X_test[key + "_cat"] = cat
+# calculate log losses again
+log_losses_2 = [
+    log_loss(y_test, models[i][0].fit(X_train, y_train).predict_proba(X_test))
+    if models[i][1] == "proba" else
+    log_loss(y_test, models[i][0].fit(X_train, y_train)._predict_proba_lr(X_test))
+    for i in tqdm(range(len(models)))
+]
 # todo: cleaner columns names for categories
 # fit models
 # pca
 # hyperparameter tuning
 # xgboost
+
+# hypertuning for extra trees
+param_grid = {
+    "n_estimators": np.arange(50, 700, 50),
+    "max_depth": np.arange(1, 21),
+    "min_samples_split": np.arange(1, 21)
+}
+base_estimator = ExtraTreesClassifier(random_state=42)
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_train, y_train)
+best_estimators = []
+best_estimators.append((sh.best_estimator_, "proba"))
+# hypertuning for random forest
+param_grid = {
+    "n_estimators": np.arange(50, 700, 50),
+    "max_depth": np.arange(1, 21),
+    "min_samples_split": np.arange(1, 21)
+}
+base_estimator = RandomForestClassifier(random_state=42)
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_train, y_train)
+best_estimators.append((sh.best_estimator_, "proba"))
+# hypertuning for k neighbors
+param_grid = {
+    "n_neighbors": np.arange(2, 21, 1)
+}
+base_estimator = KNeighborsClassifier()
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_train, y_train)
+best_estimators.append((sh.best_estimator_, "proba"))
+# hypertuning for xgboost
+param_grid = {
+    'max_depth': np.arange(1, 21),
+    'learning_rate': stats.uniform(0.01, 0.1),
+    'subsample': stats.uniform(0.5, 1),
+    'n_estimators': np.arange(50, 700, 50),
+}
+base_estimator = xgb.XGBClassifier(objective='multi:softmax', seed=42)
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_train, y_train)
+best_estimators.append((sh.best_estimator_, "proba"))
+# add the remaining estimators
+best_estimators.extend(
+    [(BernoulliNB(), "proba"), (LinearDiscriminantAnalysis(), "proba"),
+    (QuadraticDiscriminantAnalysis(), "proba"), (LinearSVC(multi_class="crammer_singer"), "proba_lr"),
+    (LogisticRegression(multi_class="multinomial"), "proba"), (RidgeClassifier(), "proba_lr")
+])
+# calculate all the log losses
+log_losses_3 = [
+    log_loss(y_test, best_estimators[i][0].fit(X_train, y_train).predict_proba(X_test))
+    if best_estimators[i][1] == "proba" else
+    log_loss(y_test, best_estimators[i][0].fit(X_train, y_train)._predict_proba_lr(X_test))
+    for i in tqdm(range(len(best_estimators)))
+]
+print(log_losses_3)
+# apply oversampling
+X_resampled, y_resampled = SMOTE().fit_resample(X_train, y_train)
+# hypertuning for extra trees
+param_grid = {
+    "n_estimators": np.arange(50, 700, 50),
+    "max_depth": np.arange(1, 21),
+    "min_samples_split": np.arange(1, 21)
+}
+base_estimator = ExtraTreesClassifier(random_state=42)
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_resampled, y_resampled)
+best_estimators_2 = []
+best_estimators_2.append((sh.best_estimator_, "proba"))
+# hypertuning for random forest
+param_grid = {
+    "n_estimators": np.arange(50, 700, 50),
+    "max_depth": np.arange(1, 21),
+    "min_samples_split": np.arange(1, 21)
+}
+base_estimator = RandomForestClassifier(random_state=42)
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_resampled, y_resampled)
+best_estimators_2.append((sh.best_estimator_, "proba"))
+# hypertuning for k neighbors
+param_grid = {
+    "n_neighbors": np.arange(2, 21, 1)
+}
+base_estimator = KNeighborsClassifier()
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_resampled, y_resampled)
+best_estimators_2.append((sh.best_estimator_, "proba"))
+# hypertuning for xgboost
+param_grid = {
+    'max_depth': np.arange(1, 21),
+    'learning_rate': stats.uniform(0.01, 0.1),
+    'subsample': stats.uniform(0.5, 1),
+    'n_estimators': np.arange(50, 1050, 50),
+}
+base_estimator = xgb.XGBClassifier(objective='multi:softmax', seed=42)
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_resampled, y_resampled)
+best_estimators_2.append((sh.best_estimator_, "proba"))
+# add the remaining estimators
+best_estimators_2.extend(
+    [(BernoulliNB(), "proba"), (LinearDiscriminantAnalysis(), "proba"),
+    (QuadraticDiscriminantAnalysis(), "proba"), (LinearSVC(multi_class="crammer_singer"), "proba_lr"),
+    (LogisticRegression(multi_class="multinomial"), "proba"), (RidgeClassifier(), "proba_lr")
+])
+log_losses_4 = [
+    log_loss(y_test, best_estimators_2[i][0].fit(X_train, y_train).predict_proba(X_test))
+    if best_estimators_2[i][1] == "proba" else
+    log_loss(y_test, best_estimators_2[i][0].fit(X_train, y_train)._predict_proba_lr(X_test))
+    for i in tqdm(range(len(best_estimators_2)))
+]
+X_resampled_2, y_resampled_2 = ADASYN().fit_resample(X, y)
+# hypertuning for extra trees
+param_grid = {
+    "n_estimators": np.arange(50, 1050, 50),
+    "max_depth": np.arange(1, 21),
+    "min_samples_split": np.arange(1, 21)
+}
+base_estimator = ExtraTreesClassifier(random_state=42)
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_resampled_2, y_resampled_2)
+best_estimators_3 = []
+best_estimators_3.append((sh.best_estimator_, "proba"))
+# hypertuning for random forest
+param_grid = {
+    "n_estimators": np.arange(50, 1050, 50),
+    "max_depth": np.arange(1, 21),
+    "min_samples_split": np.arange(1, 21)
+}
+base_estimator = RandomForestClassifier(random_state=42)
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_resampled_2, y_resampled_2)
+best_estimators_3.append((sh.best_estimator_, "proba"))
+# hypertuning for k neighbors
+param_grid = {
+    "n_neighbors": np.arange(2, 21, 50)
+}
+base_estimator = KNeighborsClassifier()
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_resampled_2, y_resampled_2)
+best_estimators_3.append((sh.best_estimator_, "proba"))
+# hypertuning for xgboost
+param_grid = {
+    'max_depth': np.arange(1, 21),
+    'learning_rate': stats.uniform(0.01, 0.1),
+    'subsample': stats.uniform(0.5, 1),
+    'n_estimators': np.arange(50, 1050, 50),
+}
+base_estimator = xgb.XGBClassifier(objective='multi:softmax', seed=42)
+sh = HalvingRandomSearchCV(
+    base_estimator, param_grid, cv=3,
+    factor=2, verbose=10
+).fit(X_resampled_2, y_resampled_2)
+best_estimators_3.append((sh.best_estimator_, "proba"))
+# add the remaining estimators
+best_estimators_3.extend(
+    [(BernoulliNB(), "proba"), (LinearDiscriminantAnalysis(), "proba"),
+    (QuadraticDiscriminantAnalysis(), "proba"), (LinearSVC(multi_class="crammer_singer"), "proba_lr"),
+    (LogisticRegression(multi_class="multinomial"), "proba"), (RidgeClassifier(), "proba_lr")
+])
+log_losses_5 = [
+    log_loss(y_test, best_estimators_3[i][0].fit(X_train, y_train).predict_proba(X_test))
+    if best_estimators_3[i][1] == "proba" else
+    log_loss(y_test, best_estimators_3[i][0].fit(X_train, y_train)._predict_proba_lr(X_test))
+    for i in tqdm(range(len(best_estimators_3)))
+]
+
+classifier = best_estimators_3[1][0]
+classifier.fit(X_train, y_train)
+test = pd.read_csv("S3_26/test.csv")
+test["Stage"] = test.Stage.astype("category")
+# add dummy variables
+test = pd.concat([test, pd.get_dummies(test.Drug, prefix="Drug")], axis=1).drop(columns="Drug")
+test = pd.concat([test, pd.get_dummies(test.Sex, prefix="Sex")], axis=1).drop(columns="Sex")
+test = pd.concat([test, pd.get_dummies(test.Ascites, prefix="Ascites")], axis=1).drop(columns="Ascites")
+test = pd.concat([test, pd.get_dummies(test.Hepatomegaly, prefix="Hepatomegaly")], axis=1).drop(columns="Hepatomegaly")
+test = pd.concat([test, pd.get_dummies(test.Spiders, prefix="Spiders")], axis=1).drop(columns="Spiders")
+test = pd.concat([test, pd.get_dummies(test.Edema, prefix="Edema")], axis=1).drop(columns="Edema")
+test = pd.concat([test, pd.get_dummies(test.Stage, prefix="Stage")], axis=1).drop(columns="Stage")
+probs = pd.DataFrame(classifier.predict_proba(test.drop(columns="id")))
+probs.columns = ["Status_" + c for c in le.classes_]
+df = pd.concat([test[["id"]], probs], axis=1)
+df.to_csv("S3_26/out.csv", index=False)
+# todo: pca
+# data wrangling for test data as well
+# decrease test / train split
+# check if cat variable over normal improves it
+# include original data
+# rewrite as classes
